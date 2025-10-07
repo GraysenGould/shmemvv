f23697170729295b8caaf97484cba7c2ac2b6d75
Graysen Gould <gouldgraysen@gmail.com>
2025-10-06 21:14:00 -0500

    updated and added tests

--- src/unit/c11/atomics/c11_shmem_atomic_inc.c
+++ src/unit/c11/atomics/c11_shmem_atomic_inc.c
@@ -11,6 +11,7 @@
 
 #include "log.h"
 #include "shmemvv.h"
+#include "type_tables.h"
 
 #define TEST_C11_SHMEM_ATOMIC_INC(TYPE)                                        \
   ({                                                                           \
@@ -38,6 +39,40 @@
                "(%d + 1 = %d)",                                                \
                #TYPE, (void *)dest, (int)value, (int)*dest);                   \
     shmem_free(dest);                                                          \
+    success;                                                                   \
+  })
+
+#define ATOMIC_TEST_C11_SHMEM_ATOMIC_INC(TYPE)                                 \
+  ({                                                                           \
+    log_routine("shmem_atomic_inc(" #TYPE ") atomic test");                    \
+    bool success = true;                                                       \
+    int my_pe, npes;                                                           \
+    npes = shmem_n_pes();                                                      \
+    my_pe = shmem_my_pe();                                                     \
+    int count = 100000;                                                        \
+    static TYPE *counter;                                                      \
+    counter = (TYPE *)shmem_malloc(sizeof(TYPE));                              \
+    *counter = 0;                                                              \
+    log_info("shmem_malloc'd %d bytes at %p", sizeof(TYPE), (void *)counter);  \
+    shmem_barrier_all();                                                       \
+    log_info("PE %d: Executing atomic increments",my_pe);                      \
+    for (int i = 0; i < count; i ++){                                          \
+        shmem_atomic_inc(counter, 0);                                          \
+    }                                                                          \
+    shmem_quiet(); \
+    shmem_barrier_all();                                                       \
+    if (my_pe == 0){                                                           \
+        log_info("PE 0: Validating incrementing results");                     \
+        log_info("Expected count: %d, Actual: %d", (npes  * count), *counter);\
+        if (*counter == (npes * count)){                                       \
+            log_info("PE 0: Count is correct: %d",*counter);                   \
+        }                                                                      \
+        else{                                                                  \
+            log_fail("PE 0: Count is not correct: %d", *counter);              \
+            success = 0;                                                       \
+        }                                                                      \
+    }                                                                          \
+    shmem_free(counter);                                                       \
     success;                                                                   \
   })
 
@@ -84,6 +119,50 @@
     success;                                                                   \
   })
 
+  #define ATOMIC_TEST_C11_CTX_SHMEM_ATOMIC_INC(TYPE)                           \
+  ({                                                                           \
+    log_routine("shmem_atomic_inc(ctx, " #TYPE ") atomic test");               \
+    bool success = true;                                                       \
+    int my_pe, npes;                                                           \
+    npes = shmem_n_pes();                                                      \
+    my_pe = shmem_my_pe();                                                     \
+    int count = 100000;                                                        \
+    static TYPE *counter;                                                      \
+    counter = (TYPE *)shmem_malloc(sizeof(TYPE));                              \
+    *counter = 0;                                                              \
+    log_info("shmem_malloc'd %d bytes at %p", sizeof(TYPE), (void *)counter);  \
+    shmem_ctx_t ctx;                                                           \
+    int ctx_create_status = shmem_ctx_create(0, &ctx);                         \
+    if (ctx_create_status != 0) {                                              \
+      log_fail("Failed to create context");                                    \
+      shmem_free(counter);                                                     \
+      return false;                                                            \
+    }                                                                          \
+    log_info("Successfully created context");                                  \
+    shmem_barrier_all();                                                       \
+    log_info("PE %d: Executing atomic increments",my_pe);                      \
+    for (int i = 0; i < count; i ++){                                          \
+        shmem_atomic_inc(ctx, counter, 0);                                     \
+    }                                                                          \
+    shmem_ctx_quiet(ctx); \
+    shmem_barrier_all();                                                       \
+    if (my_pe == 0){                                                           \
+        log_info("PE 0: Validating incrementing results");                     \
+        log_info("Expected count: %d, Actual: %d", (npes * count), *counter);  \
+        if (*counter == (npes * count)){                                       \
+            log_info("PE 0: Count is correct: %d",*counter);                   \
+        }                                                                      \
+        else{                                                                  \
+            log_fail("PE 0: Count is not correct: %d", *counter);              \
+            success = 0;                                                       \
+        }                                                                      \
+    }                                                                          \
+    shmem_ctx_destroy(ctx);                                                    \
+    log_info("Context destroyed");                                             \
+    shmem_free(counter);                                                       \
+    success;                                                                   \
+  })
+
 int main(int argc, char *argv[]) {
   shmem_init();
   log_init(__FILE__);
@@ -92,40 +171,45 @@ int main(int argc, char *argv[]) {
 
   /* Test standard atomic inc operations */
   bool result = true;
-  result &= TEST_C11_SHMEM_ATOMIC_INC(int);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(long);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(long long);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(unsigned int);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(unsigned long);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(unsigned long long);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(int32_t);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(int64_t);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(uint32_t);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(uint64_t);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(size_t);
-  result &= TEST_C11_SHMEM_ATOMIC_INC(ptrdiff_t);
+
+  #define X(type, shmem_types) result &= TEST_C11_SHMEM_ATOMIC_INC(type);
+    SHMEM_STANDARD_AMO_TYPE_TABLE(X)
+  #undef X
 
   if (shmem_my_pe() == 0) {
     display_test_result("C11 shmem_atomic_inc", result, false);
   }
 
+  /* Test atomic guarentee of standard routine */
+  bool atomic_result = true;
+  #define X(type, shmem_types) atomic_result &= ATOMIC_TEST_C11_SHMEM_ATOMIC_INC(type);
+    SHMEM_STANDARD_AMO_TYPE_TABLE(X)
+  #undef X
+
+  if (shmem_my_pe() == 0) {
+    display_test_result("C11 shmem_atomic_inc atomic test", atomic_result, false);
+  }
+
   /* Test context-specific atomic inc operations */
   bool result_ctx = true;
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(int);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(long);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(long long);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(unsigned int);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(unsigned long);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(unsigned long long);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(int32_t);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(int64_t);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(uint32_t);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(uint64_t);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(size_t);
-  result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(ptrdiff_t);
+
+  #define X(type, shmem_types) result_ctx &= TEST_C11_CTX_SHMEM_ATOMIC_INC(type);
+    SHMEM_STANDARD_AMO_TYPE_TABLE(X)
+  #undef X
 
   if (shmem_my_pe() == 0) {
     display_test_result("C11 shmem_atomic_inc with ctx", result_ctx, false);
+  }
+
+  /* Test atomics context-specific atomic inc operations */
+  bool result_ctx_atomic = true;
+
+  #define X(type, shmem_types) result_ctx_atomic &= ATOMIC_TEST_C11_CTX_SHMEM_ATOMIC_INC(type);
+    SHMEM_STANDARD_AMO_TYPE_TABLE(X)
+  #undef X
+
+  if (shmem_my_pe() == 0) {
+    display_test_result("C11 shmem_atomic_inc atomic test with ctx", result_ctx_atomic, false);
   }
 
   if (!result || !result_ctx) {
